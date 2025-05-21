fe9bbd7d0c4260cb943ec9024d4f2b7a47f7dc4a
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2025-05-21 11:25:12 +0500

    Create intro.py
    1-lesson : Introduction

---
+++ intro.py
@@ -0,0 +1,70 @@
+"""Bu bizni Pythondagi birinchi kodimiz"""
+# Interpretator - kodni tog'ri yozilganiga tekshiruvchi dastur
+# Kompilyator - kodni mashina kodiga tarjima qiladi
+
+# print("hello world")
+
+# Komentlar - qisqa , lo'nda, mantiqan to'gri va ingliz tilida bo'lishi kerak
+
+# Dastur
+# 1. Ma'lumot qabul qilish 
+# 2.Uni qayta ishlash
+# 3.Saqlash 
+# 4.Natijani qaytarish 
+
+# O'zgaruvchi 
+# Malumot turlari 
+
+# O'zgaruvchi - kompyuter xotirasidagi RAM ma'lum bir katakchaga olib boruvchi manzil 
+# def main():
+#     pass 
+
+# print(main)  # <function main at 0x0000025194F704A0>
+# main  = 0x0000025194F704A0
+
+# x = 10
+
+# print(x)
+
+# z = 3
+
+# print(10+10)
+
+# O'zgaruvchini nomlashda :
+# 1.ingliz tilida 
+# 2.mantiqan to'gri nom
+# 3.birinchi belgisi raqam yoki belgilar bolishi mumkin emas faqat _ mumkin
+
+# Mumkin:
+#     name 
+#     NAME 
+#     n1
+#     _name 
+    
+# Mumkin emas:
+#     1name
+#     %name
+#     n 
+
+# Case'lar - register
+# Camel case > userAge , userProfilePictureUrl 
+# Snake case > user_age,user_profile_picture_url
+
+# cd documents 
+# cd Documents
+# NAME = "john"
+# name = "david"
+# print(NAME,name)
+
+
+# PI = 3.14
+# PI = 3.50
+# print(PI) 
+
+# x = 10
+# x = 15
+# print(x)
+
+import keyword
+print(keyword.kwlist)
+# ['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
