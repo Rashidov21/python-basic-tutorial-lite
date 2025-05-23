28171f3cae39abf40071fd727721266e11d2ad49
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2025-05-23 11:28:43 +0500

    Create work_with_dirs.py

---
+++ mini-projects/work_with_dirs.py
@@ -0,0 +1,18 @@
+import os
+import shutil
+# print(os.getcwd()) # C:\Users\rashi\Documents\GitHub\python-basic-tutorial-lite
+
+# os.mkdir('test1') # papka hosil qilish
+# os.rmdir('test1') # papkani o'chirish (faqat bo'sh bo'lsa)
+# os.makedirs("test1/test1_2/test1_2_3")
+# os.rmdir("test1") # 
+# shutil.rmtree("test1") # agar papka ichida papkalar va fayllar bolsa o'chirish uchun 
+
+# Joriy katalogdagi barcha fayl va papkalarni ko'rish
+# print(os.listdir()) # ['.git', 'data_types.py', 'intro.py', 'mini-projects']
+
+# yangi fayl hosil qilish 
+# open("test.txt", "w")
+
+# faylni o'chirish
+# os.remove("test.txt")
