d53c5efeec46273d7342ebc0fa0ac02b6c847c6e
Abdurakhmon Rashidov <60424017+Rashidov21@users.noreply.github.com>
2025-06-02 12:00:11 +0500

    Create work_with_numbers.py

---
+++ work_with_numbers.py
@@ -0,0 +1,60 @@
+import math 
+import random 
+
+# int, float 
+
+# str_num = "10.3"
+# float_num = float(str_num)
+# print(float_num)
+
+# str_num = "10"
+# int_num = int(str_num)
+# print(int_num)
+
+# round - yaxlitlash
+# n = 1.25664
+# print(round(n)) # 1
+# n = 1.85664
+# print(round(n)) # 2
+
+# x = 7.2654668
+# print(round(x,2)) # 7.27
+# print(round(x,1)) # 7.3
+
+# print(2,abs(-2))
+# print(abs(5)) # 5
+
+# pow - daraja 
+# print(pow(2,10)) # 1024
+
+# # min , max - minimum yoki maksimum olish 
+# print(min([1,2,3])) # 1
+# print(max([1,2,3])) # 3
+
+# print(sum([1,2,3])) # 6
+
+# print(math.pi) # 3.141592653589793
+
+# print(math.floor(7.99999)) # 7
+# print(math.ceil(7.003)) # 8
+# print(math.pow(2,8)) # 256.0
+# print(math.sqrt(16)) # 4.0
+
+# print(random.randint(1,10)) 
+# print(random.randrange(1,10)) 
+
+# shuffle - berilgan ketma-ketlik elementlarini aralashtirish
+# l = [1,2,3,4,5]
+# random.shuffle(l)
+# print(l)
+
+# sample - berilgan ketma-ketlik elementlari ichidan siz korsatgan miqdorda tasodiflarini olib array qilib qaytaradi 
+# print(random.sample("salom",3))
+
+# choice - berilgan ketma-ketlik elementlari ichidan tasodify bittasini olish 
+# print(random.choice("salom"))
+
+# task 
+# user belgilar sonini (n < 6) kiritadi siz shuncha tasodiy belgidan iborat parol hosil qilishingiz kerak
+# input: 7
+# output: Fy6_rs1
